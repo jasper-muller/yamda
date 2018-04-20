515cc2c249ea27c554d9a3291a4eabf329035187
Jasper Muller <jasper.muller@eneco.com>
2018-04-20 15:21:53 +0200

    Changed settings

--- jamuda_site/settings.py
+++ jamuda_site/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv, find_dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +21,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#=m!*(=v)12_2@mi6lr&8893udpu!lh(jq%x25lw=9qd2&=tt&'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.getenv("DEBUG") == "True")
 
-ALLOWED_HOSTS = ['159.65.205.205']
+ALLOWED_HOSTS = [
+    'yamda.nl'
+]
 
 
 # Application definition
@@ -77,14 +80,13 @@ WSGI_APPLICATION = 'jamuda_site.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': '',
-        'USER': 'u_yamda',
-        'PASSWORD': '',
+        'NAME': 'yamda',
+        'USER': os.getenv("YAMDA_USER"),
+        'PASSWORD': os.getenv("YAMDA_KEY"),
         'HOST': 'localhost',
         'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
@@ -123,4 +125,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '../static')
